eef63ecc69ce64db53a417f6e87ed6d96df2dc02
Moritz <morationisch@gmail.com>
2023-07-27 13:15:34 +0200

    added multithreading for the screen updates. But it's still a bit laggy ...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-
+import threading
 import pygame as pg
 from win32api import GetSystemMetrics
 from resources.resource_loader import SaveMng
@@ -9,6 +9,7 @@ from resources.Maps.map_generator import MapGen
 import time
 
 grey = (50, 50, 50)
+running = True
 
 def key_pressed(input_key):
     keys_pressed = pg.key.get_pressed()
@@ -18,31 +19,24 @@ def key_pressed(input_key):
         return False
 
 def main():
+    global running
     starting_map = "Farm"
-    scale = 50
-    target_fps = 65
+    scale = 80
+    target_fps = 53
     frame_t_time = 1000000000 / target_fps
     save_name = "save1"
-    MapGen()
+    # MapGen()
     pg.init()
     screen_size = (GetSystemMetrics(0), GetSystemMetrics(1))
     root = pg.display.set_mode(screen_size, pg.FULLSCREEN)
     pg.display.toggle_fullscreen()
-    sprite_loader = SaveMng(scale=scale, entity_scale=50, item_scale=10)
+    sprite_loader = SaveMng(scale=scale, entity_scale=80, item_scale=10)
     cur_map = Map(sprite_loader, starting_map, save_name, scale)
     player = Player(cur_map.player_start_pos, screen_size, sprite_loader, cur_map)
-    s_time = time.time_ns()
-    while True:
-        frame_time = time.time_ns() - s_time
-        if frame_time < frame_t_time:
-            time.sleep((frame_t_time - frame_time) / 1000000000)
-        frame_time = time.time_ns() - s_time
-        if frame_time != 0:
-            print(math.floor(1000000000 / frame_time))
-        s_time = time.time_ns()
-        root.fill(grey)
+    screen_thread = threading.Thread(target=screen_threading, args=(root,player, screen_size, scale, cur_map,))
+    screen_thread.start()
+    while running:
 
-        root = cur_map.update(root, player, screen_size, scale=50)
         pg.display.update()
 
 
@@ -50,6 +44,7 @@ def main():
         for event in pg.event.get():
             match event.type:
                 case pg.QUIT:
+                    running = False
                     exit()
 
                 case pg.KEYDOWN:
@@ -86,9 +81,25 @@ def main():
 
 
 
+def screen_threading(root, player, screen_size, scale, cur_map):
+    global running
+    target_fps = 144
+    frame_t_time = 1000000000 / target_fps
+    s_time = time.time_ns()
+    while running:
+        frame_time = time.time_ns() - s_time
+        if frame_time > frame_t_time:
+            s_time = time.time_ns()
+            cur_map.update(root, player, screen_size, scale)
+
+        if frame_time != 0:
+            print(math.floor(1000000000 / frame_time))
+
 
 
 def esc_menu():
+    global running
+    running = False
     exit()                      # TODO: here we need to make an escape menu !
 
 
